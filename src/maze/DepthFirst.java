9fe769b0d7ee3dc851800729d5c2a4c53b4f5a40
Mihir9702 <mkpatel9702@gmail.com>
2023-10-13 09:24:38 -0400

    v1.0

---
+++ src/maze/DepthFirst.java
@@ -0,0 +1,63 @@
+package maze;
+
+import java.util.List;
+
+public class DepthFirst {
+
+  // if path is found this method returns true
+  // path: { xn, yb, ... x-1, y-1, x0, y0 }
+  // order is from end to start (reverse)
+  public static boolean searchPath(
+    int[][] maze,
+    int x,
+    int y,
+    List<Integer> path
+  ) {
+    // 0 - not-visited
+    // 2 - visited
+
+    if (maze[y][x] == 9) {
+      path.add(x);
+      path.add(y);
+      return true;
+    }
+
+    if (maze[y][x] == 0) {
+      maze[y][x] = 2;
+
+      int dx = -1;
+      int dy = 0;
+      if (searchPath(maze, x + dx, y + dy, path)) {
+        path.add(x);
+        path.add(y);
+        return true;
+      }
+
+      dx = 1;
+      dy = 0;
+      if (searchPath(maze, x + dx, y + dy, path)) {
+        path.add(x);
+        path.add(y);
+        return true;
+      }
+
+      dx = 0;
+      dy = -1;
+      if (searchPath(maze, x + dx, y + dy, path)) {
+        path.add(x);
+        path.add(y);
+        return true;
+      }
+
+      dx = 0;
+      dy = 1;
+      if (searchPath(maze, x + dx, y + dy, path)) {
+        path.add(x);
+        path.add(y);
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
--- src/maze/Window.java
+++ src/maze/Window.java
@@ -1,14 +1,113 @@
 package maze;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Window extends JFrame {
+
+  private int[][] maze = {
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1 },
+    { 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1 },
+    { 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1 },
+    { 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1 },
+    { 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 9, 1 }, // (11, 8) t
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+  };
+  private final List<Integer> path = new ArrayList<Integer>();
+  private int pathIndex;
 
   Window(int width, int height) {
     setTitle("Maze Solver");
     setSize(width, height);
-    setVisible(true);
+    setLocationRelativeTo(null);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     setResizable(false);
+
+    DepthFirst.searchPath(maze, 1, 1, path);
+    pathIndex = path.size() - 2;
+    System.out.println(path);
+  }
+
+  @Override
+  public void paint(Graphics g) {
+    // TODO Auto-generated method stub
+    super.paint(g);
+
+    g.translate(50, 50);
+
+    for (int row = 0; row < maze.length; row++) {
+      for (int col = 0; col < maze[row].length; col++) {
+        Color color;
+        switch (maze[row][col]) {
+          case 1:
+            color = Color.BLACK;
+            break;
+          case 9:
+            color = Color.RED;
+            break;
+          default:
+            color = Color.WHITE;
+            break;
+        }
+
+        g.setColor(color);
+        g.fillRect(col * 30, row * 30, 30, 30);
+        g.setColor(Color.BLACK);
+        g.drawRect(col * 30, row * 30, 30, 30);
+      }
+    }
+
+    // draw path
+    for (int p = 0; p < path.size(); p += 2) {
+      int px = path.get(p);
+      int py = path.get(p + 1);
+      g.setColor(Color.GREEN);
+      g.fillRect(px * 30, py * 30, 30, 30);
+    }
+
+    // draw current position
+    int px = path.get(pathIndex);
+    int py = path.get(pathIndex + 1);
+    g.setColor(Color.BLUE);
+    g.fillRect(px * 30, py * 30, 30, 30);
+  }
+
+  @Override
+  protected void processKeyEvent(KeyEvent e) {
+    if (e.getID() != KeyEvent.KEY_PRESSED) {
+      return;
+    }
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+      pathIndex -= 2;
+      if (pathIndex < 0) {
+        pathIndex = 0;
+      }
+    } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+      pathIndex += 2;
+      if (pathIndex >= path.size()) {
+        pathIndex = path.size() - 2;
+      }
+    }
+    repaint();
+  }
+
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(
+      new Runnable() {
+        @Override
+        public void run() {
+          Window window = new Window(640, 480);
+          window.setVisible(true);
+        }
+      }
+    );
   }
 }
